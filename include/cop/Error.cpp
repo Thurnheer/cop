41d49eea561dd30532fa6cba43eccdb843cf853b
Christoph Thurnheer <c.thurnheer@gmx.ch>
2020-10-17 18:56:30 +0200

    try to fix windows build

--- include/cop/Error.cpp
+++ include/cop/Error.cpp
@@ -16,7 +16,7 @@ const char* ProtocolErrCategory::name() const noexcept
 
 std::string ProtocolErrCategory::message(int ev) const {
     switch(static_cast<cop::ProtocolErrc>(ev)) {
-        case invalid_message_id:
+        case ProtocolErr::invalid_message_id:
             break;
         default:
             return "(unrecognised error)";
